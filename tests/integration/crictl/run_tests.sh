accb12474b905388cc43033edbaded94630ecdc0
Wainer dos Santos Moschetta <wainersm@redhat.com>
2023-10-06 15:49:51 -0300

    tests/crictl: convert TestContainerSwap to bats
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- tests/integration/crictl/run_tests.sh
+++ tests/integration/crictl/run_tests.sh
@@ -31,8 +31,11 @@ export CONTAINERD_CONFIG_FILE="${tmp_dir}/test-containerd-config"
 export CONTAINERD_CONFIG_FILE_TEMP="${CONTAINERD_CONFIG_FILE}.temp"
 export default_containerd_config_backup="$CONTAINERD_CONFIG_FILE.backup"
 
-TESTS_UNION=(generic.bats \
-	container_update.bats)
+TESTS_UNION=(
+	generic.bats \
+	container_update.bats\
+	container_swap.bats \
+	)
 
 function cleanup() {
 	ci_cleanup
@@ -53,140 +56,6 @@ function err_report() {
 	sudo journalctl -xe -t kata
 	echo "-------------------------------------"
 	echo "::endgroup::"
-}
-
-function getContainerSwapInfo() {
-	swap_size=$(($(sudo crictl exec $cid cat /proc/meminfo | grep "SwapTotal:" | awk '{print $2}')*1024))
-	# NOTE: these below two checks only works on cgroup v1
-	swappiness=$(sudo crictl exec $cid cat /sys/fs/cgroup/memory/memory.swappiness)
-	swap_in_bytes=$(sudo crictl exec $cid cat /sys/fs/cgroup/memory/memory.memsw.limit_in_bytes)
-}
-
-function TestContainerSwap() {
-	if [[ "${KATA_HYPERVISOR}" != "qemu" ]] || [[ "${ARCH}" != "x86_64" ]]; then
-		return
-	fi
-
-	local container_yaml=${REPORT_DIR}/container.yaml
-	local image="busybox:latest"
-
-	info "Test container with guest swap"
-
-	create_containerd_config "kata-${KATA_HYPERVISOR}" 1
-	sudo sed -i -e 's/^#enable_guest_swap.*$/enable_guest_swap = true/g' "${kata_config}"
-
-	# Test without swap device
-	testContainerStart
-	getContainerSwapInfo
-	# Current default swappiness is 60
-	if [ $swappiness -ne 60 ]; then
-		testContainerStop
-		die "The VM swappiness $swappiness without swap device is not right"
-	fi
-	if [ $swap_in_bytes -lt 1125899906842624 ]; then
-		testContainerStop
-		die "The VM swap_in_bytes $swap_in_bytes without swap device is not right"
-	fi
-	if [ $swap_size -ne 0 ]; then
-		testContainerStop
-		die "The VM swap size $swap_size without swap device is not right"
-	fi
-	testContainerStop
-
-	# Test with swap device
-	cat << EOF > "${container_yaml}"
-metadata:
-  name: busybox-swap
-  namespace: default
-  uid: busybox-swap-uid
-annotations:
-  io.katacontainers.container.resource.swappiness: "100"
-  io.katacontainers.container.resource.swap_in_bytes: "1610612736"
-linux:
-  resources:
-    memory_limit_in_bytes: 1073741824
-image:
-  image: "$image"
-command:
-- top
-EOF
-
-	testContainerStart 1
-	getContainerSwapInfo
-	testContainerStop
-
-	if [ $swappiness -ne 100 ]; then
-		die "The VM swappiness $swappiness with swap device is not right"
-	fi
-	if [ $swap_in_bytes -ne 1610612736 ]; then
-		die "The VM swap_in_bytes $swap_in_bytes with swap device is not right"
-	fi
-	if [ $swap_size -ne 536870912 ]; then
-		die "The VM swap size $swap_size with swap device is not right"
-	fi
-
-	# Test without swap_in_bytes
-	cat << EOF > "${container_yaml}"
-metadata:
-  name: busybox-swap
-  namespace: default
-  uid: busybox-swap-uid
-annotations:
-  io.katacontainers.container.resource.swappiness: "100"
-linux:
-  resources:
-    memory_limit_in_bytes: 1073741824
-image:
-  image: "$image"
-command:
-- top
-EOF
-
-	testContainerStart 1
-	getContainerSwapInfo
-	testContainerStop
-
-	if [ $swappiness -ne 100 ]; then
-		die "The VM swappiness $swappiness without swap_in_bytes is not right"
-	fi
-	# swap_in_bytes is not set, it should be a value that bigger than 1125899906842624
-	if [ $swap_in_bytes -lt 1125899906842624 ]; then
-		die "The VM swap_in_bytes $swap_in_bytes without swap_in_bytes is not right"
-	fi
-	if [ $swap_size -ne 1073741824 ]; then
-		die "The VM swap size $swap_size without swap_in_bytes is not right"
-	fi
-
-	# Test without memory_limit_in_bytes
-	cat << EOF > "${container_yaml}"
-metadata:
-  name: busybox-swap
-  namespace: default
-  uid: busybox-swap-uid
-annotations:
-  io.katacontainers.container.resource.swappiness: "100"
-image:
-  image: "$image"
-command:
-- top
-EOF
-
-	testContainerStart 1
-	getContainerSwapInfo
-	testContainerStop
-
-	if [ $swappiness -ne 100 ]; then
-		die "The VM swappiness $swappiness without memory_limit_in_bytes is not right"
-	fi
-	# swap_in_bytes is not set, it should be a value that bigger than 1125899906842624
-	if [ $swap_in_bytes -lt 1125899906842624 ]; then
-		die "The VM swap_in_bytes $swap_in_bytes without memory_limit_in_bytes is not right"
-	fi
-	if [ $swap_size -ne 2147483648 ]; then
-		die "The VM swap size $swap_size without memory_limit_in_bytes is not right"
-	fi
-
-	create_containerd_config "kata-${KATA_HYPERVISOR}"
 }
 
 function main() {
@@ -210,11 +79,6 @@ function main() {
 
 	# trap error for print containerd and kata-containers log
 	trap err_report ERR
-
-	# TestContainerSwap is currently failing with GHA.
-	# Let's re-enable it as soon as we get it to work.
-	# Reference: https://github.com/kata-containers/kata-containers/issues/7410
-	# TestContainerSwap
 
 	pushd "$SCRIPT_PATH"
 	bats ${TESTS_UNION[@]}
